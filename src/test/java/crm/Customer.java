a34fe2884f4b1abedc5e6cdc464fb30f874e561d
Chella001 <chelladurai@logimaxindia.com>
2026-05-18 17:39:45 +0530

    New customer by AI

--- src/test/java/crm/Customer.java
+++ src/test/java/crm/Customer.java
@@ -8,7 +8,9 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.Select;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 import utilsCrm.crmFunctions;
 import utilsCrm.crmUtils;
@@ -24,11 +26,9 @@ public class Customer extends crmDriver {
 			return "";
 		}
 
-
 		DataFormatter formatter = new DataFormatter();
 		return formatter.formatCellValue(row.getCell(cellIndex)).trim();
 	}
-
 
 	public boolean Customer(Workbook workbook, int lastRow) throws InterruptedException {
 
@@ -59,34 +59,35 @@ public class Customer extends crmDriver {
 			String Address2 = getCellValue(row, 15);
 			String Address3 = getCellValue(row, 16);
 			String Country = getCellValue(row, 17);
-			String State  = getCellValue(row, 18);
-			String City  = getCellValue(row, 19);
+			String State = getCellValue(row, 18);
+			String City = getCellValue(row, 19);
 			String Pincode = getCellValue(row, 20);
 			String Area = getCellValue(row, 21).trim();
 			String selectReligion = getCellValue(row, 22);
-			//	String Gender = getCellValue(row, 23);
-			String maritalStatus = getCellValue(row, 24);
-			// String dateOfBirth = getCellValue(row, 25);
-			//	String weddingDate = getCellValue(row, 26);
-			String Nominiee  = getCellValue(row, 27);
+			String maritalStatus = getCellValue(row, 26);
+			String Nominiee = getCellValue(row, 27);
 			String nominieeName = getCellValue(row, 28);
 			String nominieeRelation = getCellValue(row, 29);
 			String nomineeNumber = getCellValue(row, 30);
 			String nomineeAdd1 = getCellValue(row, 31);
-			String nomineeAdd2  = getCellValue(row, 32);
-			String ImageFront  = getCellValue(row, 33);
-			String PanNo  = getCellValue(row, 34);
-			String VoterId = getCellValue(row, 35);
-			String rationCard = getCellValue(row, 36);
+			String nomineeAdd2 = getCellValue(row, 32);
 
-			String Password = getCellValue(row, 62);
-			String edit = getCellValue(row, 63);
-			String delete = getCellValue(row, 64);
-			String editCustomer = getCellValue(row, 65);
-			String editMobile = getCellValue(row, 66);
-			String editMail = getCellValue(row, 67);
-			String editAdd = getCellValue(row, 68);
-			String nominieeImage = getCellValue(row, 69);
+			// Document numbers - CORRECTED column indices
+			String Password = getCellValue(row, 33);
+			// String PanNo = getCellValue(row, 34);
+			// String VoterId = getCellValue(row, 35);
+			// String rationCard = getCellValue(row, 36);
+
+			String edit = getCellValue(row, 34);
+			String delete = getCellValue(row, 35);
+			String editCustomer = getCellValue(row, 36);
+			String editMobile = getCellValue(row, 37);
+			String editMail = getCellValue(row, 38);
+			String editAdd = getCellValue(row, 39);
+			String PanNo = getCellValue(row, 40);
+			String VoterId = getCellValue(row, 41);
+			String rationCard = getCellValue(row, 42);
+			String nominieeImage = getCellValue(row, 43);
 
 			String path = "C:\\Users\\Chella\\eclipse-workspace\\mavenCrm\\Test_Images";
 
@@ -110,10 +111,10 @@ public class Customer extends crmDriver {
 							By.cssSelector("div.slimScrollDiv > section.sidebar"));
 
 					((JavascriptExecutor) driver)
-					.executeScript("arguments[0].scrollTop = 1500;", sidebar);
+							.executeScript("arguments[0].scrollTop = 1500;", sidebar);
 
-					util.clickByXpath("/html/body/div/aside[1]/div/section/ul/li[36]/a");
-					util.clickByXpath("/html/body/div/aside[1]/div/section/ul/li[36]/ul/li[3]/a");
+					util.clickByXpath("/html/body/div[1]/aside[1]/div/section/ul/li[37]/a");
+					util.clickByXpath("/html/body/div[1]/aside[1]/div/section/ul/li[37]/ul/li[3]/a");
 
 					Thread.sleep(1500);
 					navigatedToCustomer = true;
@@ -139,7 +140,7 @@ public class Customer extends crmDriver {
 					((JavascriptExecutor) driver).executeScript(
 							"$(\"input[name='customer[cus_type]'][value='2']\").prop('checked', true).trigger('change');");
 
-					util.clickByXpath("(//span[@role='presentation'])[1]");        		
+					util.clickByXpath("(//span[@role='presentation'])[1]");
 					util.enterTextByXpath("/html/body/span/span/span[1]/input", Title);
 					driver.findElement(By.xpath("/html/body/span/span/span[1]/input")).sendKeys(Keys.ENTER);
 
@@ -154,13 +155,12 @@ public class Customer extends crmDriver {
 				driver.findElement(By.xpath("/html/body/span/span/span[1]/input")).sendKeys(
 						Keys.ENTER);
 
-				System.out.println(Password);
+				// System.out.println(Password);
 				util.enterTextById("opening_bal_amt", openingBalanceAmount);
 				util.enterTextById("email", E_Mail);
 				util.enterTextByXpath("(//input[@id='phone'])[2]", Phone);
-				if (Password != "")
-				{
-					util.enterTextById("passwd" , Password);
+				if (Password != "") {
+					util.enterTextById("passwd", Password);
 				}
 				util.enterTextById("address1", Address1);
 				util.enterTextById("address2", Address2);
@@ -170,7 +170,7 @@ public class Customer extends crmDriver {
 				util.enterTextByXpath("/html/body/span/span/span[1]/input", Country);
 
 				driver.findElement(By.xpath("/html/body/span/span/span[1]/input"))
-				.sendKeys(Keys.ARROW_DOWN);
+						.sendKeys(Keys.ARROW_DOWN);
 				driver.findElement(By.xpath("/html/body/span/span/span[1]/input")).sendKeys(Keys.ENTER);
 
 				util.clickByXpath("//*[@id=\"tab_1\"]/div[1]/div[5]/div[2]/div/span/span[1]/span/span[2]");
@@ -180,16 +180,38 @@ public class Customer extends crmDriver {
 				util.clickByXpath("//*[@id=\"tab_1\"]/div[1]/div[5]/div[3]/div/span/span[1]/span/span[2]");
 				util.enterTextByXpath("/html/body/span/span/span[1]/input", City);
 				driver.findElement(By.xpath("/html/body/span/span/span[1]/input")).sendKeys(Keys.ENTER);
-
-				util.enterTextById("pin_code_add", Pincode );
-				Thread.sleep(2000);
-
-				util.clickByXpath("//*[@id=\"tab_1\"]/div[1]/div[6]/div[2]/div[1]/span/span[1]/span/span[2]");
-				util.enterTextByXpath("/html/body/span/span/span[1]/input", Area);
 				Thread.sleep(1000);
-				driver.findElement(By.xpath("/html/body/span/span/span[1]/input")).sendKeys(Keys.ENTER);
+				((JavascriptExecutor) driver).executeScript("window.scrollBy(0, 500);");
+				Thread.sleep(1000);
 
-				//Religion
+				util.enterTextById("pin_code_add", Pincode);
+				driver.findElement(By.id("pin_code_add")).sendKeys(Keys.TAB);
+				Thread.sleep(2000);
+				WebDriverWait shortWait = new WebDriverWait(driver, java.time.Duration.ofSeconds(10));
+				WebElement villageContainer = shortWait.until(ExpectedConditions.presenceOfElementLocated(
+						By.xpath("//span[@aria-labelledby='select2-Village-container']")));
+				((JavascriptExecutor) driver).executeScript("arguments[0].scrollIntoView({block: 'center'});",
+						villageContainer);
+				Thread.sleep(1000);
+				try {
+					villageContainer.click();
+				} catch (Exception e) {
+					((JavascriptExecutor) driver).executeScript(
+							"var evt = new MouseEvent('mousedown', { bubbles: true, cancelable: true, view: window }); arguments[0].dispatchEvent(evt);",
+							villageContainer);
+				}
+				try {
+					((JavascriptExecutor) driver).executeScript("$('#Village').select2('open');");
+				} catch (Exception e2) {
+				}
+				WebElement searchInput = shortWait.until(ExpectedConditions.visibilityOfElementLocated(
+						By.xpath("/html/body/span/span/span[1]/input | //input[@class='select2-search__field']")));
+				searchInput.clear();
+				searchInput.sendKeys(Area);
+				Thread.sleep(1000);
+				searchInput.sendKeys(Keys.ENTER);
+
+				// Religion
 
 				WebElement dropdown = driver.findElement(By.id("religion_select"));
 				Select select = new Select(dropdown);
@@ -198,69 +220,85 @@ public class Customer extends crmDriver {
 					select.selectByValue("1");
 				} else if (selectReligion.equalsIgnoreCase("Muslim")) {
 					select.selectByValue("2");
-				}
-				else if (selectReligion.equalsIgnoreCase("Christian")) {
+				} else if (selectReligion.equalsIgnoreCase("Christian")) {
 					select.selectByValue("3");
 				}
 
-				//Gender
+				// Gender
 
-				if(Title.equalsIgnoreCase("Mr")) 
-				{
+				if (Title.equalsIgnoreCase("Mr")) {
 					util.clickByXpath("(//input[@name='customer[gender]'])[1]");
 
-				}
-				else if (Title.equalsIgnoreCase("Mrs")) 
-				{
+				} else if (Title.equalsIgnoreCase("Mrs")) {
 					util.clickByXpath("(//input[@name='customer[gender]'])[2]");
-				}
-				else if (Title.equalsIgnoreCase("Mrs")) 
-				{
+				} else if (Title.equalsIgnoreCase("Mrs")) {
 					util.clickByXpath("(//input[@name='customer[gender]'])[3]");
 				}
 
-				//	JavascriptExecutor js = (JavascriptExecutor) driver; js.executeScript( "document.getElementById('dateField').value='15/01/2003';" );
-				if (maritalStatus.equalsIgnoreCase("Single")) {
-					util.clickByXpath("//input[@name='customer[marital_status]' and @value='1']");	
-				}
-				else if (maritalStatus.equalsIgnoreCase("Married")){
+				// JavascriptExecutor js = (JavascriptExecutor) driver; js.executeScript(
+				// "document.getElementById('dateField').value='15/01/2003';" );
+				String cleanedMarital = maritalStatus.replaceAll("\\u00A0", "").trim();
+				System.out.println(
+						"Marital Status from Excel: '" + maritalStatus + "' (Cleaned: '" + cleanedMarital + "')");
 
-					util.clickByXpath("//input[@name='customer[marital_status]' and @value='2']");
+				if (cleanedMarital.equalsIgnoreCase("Single")) {
+					try {
+						util.clickByXpath("//input[@name='customer[marital_status]' and @value='1']");
+					} catch (Exception e) {
+					}
+					((JavascriptExecutor) driver).executeScript(
+							"$(\"input[name='customer[marital_status]'][value='1']\").prop('checked', true).trigger('change');");
+				} else if (cleanedMarital.equalsIgnoreCase("Married")) {
+					try {
+						util.clickByXpath("//input[@name='customer[marital_status]' and @value='2']");
+					} catch (Exception e) {
+					}
+					((JavascriptExecutor) driver).executeScript(
+							"$(\"input[name='customer[marital_status]'][value='2']\").prop('checked', true).trigger('change');");
 				}
+
+				((JavascriptExecutor) driver).executeScript("window.scrollTo(0, document.body.scrollHeight);");
+				Thread.sleep(1000);
 
 				// Nominee Details
 
 				if (Nominiee.equalsIgnoreCase("Yes")) {
+					((JavascriptExecutor) driver).executeScript("window.scrollTo(0, 0);");
+					Thread.sleep(1000);
+
 					util.clickById("others_tab");
 					util.enterTextById("nominee_name", nominieeName);
 					util.enterTextById("nominee_relationship", nominieeRelation);
 					System.out.println(nominieeRelation);
-					util.enterTextByXpath("(//label[normalize-space(text())='Mobile']/following::input)[1]", nomineeNumber);
+					util.enterTextByXpath("(//label[normalize-space(text())='Mobile']/following::input)[1]",
+							nomineeNumber);
 					util.enterTextById("nominee_address1", nomineeAdd1);
 					util.enterTextById("nominee_address2", nomineeAdd2);
 
-					if (ImageFront.equalsIgnoreCase("PanCard")) {
+					((JavascriptExecutor) driver).executeScript("window.scrollBy(0, 600);");
+					Thread.sleep(1000);
+
+					if (PanNo != null && !PanNo.trim().isEmpty()) {
 						util.enterTextById("panno", PanNo);
 						driver.findElement(By.id("pan_proof"))
-						.sendKeys(path + "\\" + nominieeImage + ".jpg");
-
+								.sendKeys(path + "\\" + nominieeImage + ".jpg");
 					}
 
-					else if (ImageFront.equalsIgnoreCase("VoterId")) {
-						util.enterTextByXpath("customer[voterid]", VoterId);
-						//util.enterTextByXpath("//*[@id=\"panno\"]", PanNo);
-						driver.findElement(By.id("voterid_proof"))
-						.sendKeys(path + "\\" + nominieeImage + ".jpg");
-
-					} else if (ImageFront.equalsIgnoreCase("rationCard")) {
-						util.enterTextById("rationcard", rationCard);
-						//util.enterTextByXpath("//*[@id=\"panno\"]", PanNo);
-						driver.findElement(By.id("rationcard_proof"))
-						.sendKeys(path + "\\" + nominieeImage + ".jpg");
+					if (VoterId != null && !VoterId.trim().isEmpty()) {
+						util.enterTextById("voteridno", VoterId);
+						driver.findElement(By.id("voter_proof"))
+								.sendKeys(path + "\\" + nominieeImage + ".jpg");
 					}
+
+					if (rationCard != null && !rationCard.trim().isEmpty()) {
+						util.enterTextById("rationcardno", rationCard);
+						driver.findElement(By.id("ration_proof"))
+								.sendKeys(path + "\\" + nominieeImage + ".jpg");
+					}
+
+					((JavascriptExecutor) driver).executeScript("window.scrollTo(0, document.body.scrollHeight);");
+					Thread.sleep(1000);
 				}
-
-
 				// ===================== SUCCESS MESSAGE =====================
 				util.clickByXpath("//button[@type='submit']");
 				Thread.sleep(1000);
@@ -268,7 +306,7 @@ public class Customer extends crmDriver {
 				Thread.sleep(2000);
 				util.clickByXpath("//*[@id=\"cus_create\"]/div[2]/div/div/button[2]");
 				driver.navigate().refresh();
-				Thread.sleep(2000); 
+				Thread.sleep(2000);
 
 				try {
 					String searchBoxXpath = "//div[contains(@id,'customer_list_filter')]//input";
@@ -282,13 +320,20 @@ public class Customer extends crmDriver {
 					} else {
 						addSuccess = false;
 						test.fail("Customer Add Failed");
-						util.clickByXpath("button.btn.btn-default.btn-cancel");
+						try {
+							util.clickByXpath("//button[contains(@class,'btn-cancel')]");
+						} catch (Exception ex) {
+						}
 						crmFunctions.captureScreenshot("Add_Customer_Failed_" + TC_ID);
 					}
 
 				} catch (Exception e) {
 					addSuccess = false;
 					test.fail("Customer Add Verification Failed");
+					try {
+						util.clickByXpath("//button[contains(@class,'btn-cancel')]");
+					} catch (Exception ex) {
+					}
 					crmFunctions.captureScreenshot("Add_Verify_Error_" + TC_ID);
 					System.out.println("ADD VERIFY FAILED – CONTINUING");
 				}
@@ -300,8 +345,7 @@ public class Customer extends crmDriver {
 
 				// ================= DELETE =================
 				if (delete.equalsIgnoreCase("Yes")) {
-					String customerToDelete =
-							edit.equalsIgnoreCase("Yes") ? editCustomer : Mobile;
+					String customerToDelete = edit.equalsIgnoreCase("Yes") ? editCustomer : Mobile;
 					deleteSuccess = CustomerDelete(customerToDelete, TC_ID);
 				}
 
@@ -312,6 +356,10 @@ public class Customer extends crmDriver {
 
 			} catch (Exception e) {
 				finalStatus = false;
+				try {
+					util.clickByXpath("//button[contains(@class,'btn-cancel')]");
+				} catch (Exception ex) {
+				}
 				crmFunctions.captureScreenshot("Customer_Error_" + TC_ID);
 				test.fail("Unexpected Exception in Customer Add");
 			}
@@ -332,7 +380,7 @@ public class Customer extends crmDriver {
 	// ================= EDIT =================
 	public boolean CustomerEdit(String Mobile, String editCustomer, String TC_ID,
 			String editMobile, String editMail, String editAdd)
-					throws InterruptedException {
+			throws InterruptedException {
 
 		try {
 
@@ -375,7 +423,7 @@ public class Customer extends crmDriver {
 
 			if (message != null && message.toLowerCase().contains("success")) {
 				test.pass("Customer Edited Successfully : " + TC_ID);
-				return true; 
+				return true;
 			} else {
 				crmFunctions.captureScreenshot("Customer_Edit_Failed_" + TC_ID);
 				util.clickByXpath("//*[@id=\"cus_create\"]/div[2]/div/div/button[2]");
@@ -385,15 +433,17 @@ public class Customer extends crmDriver {
 
 		} catch (Exception e) {
 			crmFunctions.captureScreenshot("Customer_Edit_Exception_" + TC_ID);
+			try {
+				util.clickByXpath("//button[contains(@class,'btn-cancel')]");
+			} catch (Exception ex) {
+			}
 			test.fail("Exception during Customer Edit");
 			return false;
 		}
 	}
 
-
-
 	// ================= DELETE =================
-	public boolean CustomerDelete(String Mobile , String TC_ID)
+	public boolean CustomerDelete(String Mobile, String TC_ID)
 			throws InterruptedException {
 		try {
 			util.enterTextByXpath("//div[contains(@id,'customer_list_filter')]//input", Mobile);
@@ -402,11 +452,12 @@ public class Customer extends crmDriver {
 			util.clickByXpath("//*[@id=\"customer_list\"]/tbody/tr[1]/td[26]/div/ul/li[2]/a");
 			util.clickByXpath("//*[@id=\"confirm-delete\"]/div/div/div[3]/a");
 			Thread.sleep(1000);
-			String message = util.getSuccessMessage("/html/body/div[1]/div[1]/section[2]/div/div/div/div[2]/div[3]/div/div");
+			String message = util
+					.getSuccessMessage("/html/body/div[1]/div[1]/section[2]/div/div/div/div[2]/div[3]/div/div");
 
 			if (message != null && message.toLowerCase().contains("success")) {
 				test.pass("Customer Deleted Successfully : " + TC_ID);
-				return true; 
+				return true;
 			} else {
 				crmFunctions.captureScreenshot("Customer_Edit_Failed_" + TC_ID);
 				test.fail("Customer Delete Failed");
