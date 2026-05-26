bee93d268e398a3e5004b8924090903deaa86843
Chella001 <chelladurai@logimaxindia.com>
2026-05-26 10:39:58 +0530

    comment

--- src/test/java/crm/Payment.java
+++ src/test/java/crm/Payment.java
@@ -16,8 +16,8 @@ import org.openqa.selenium.Keys;
 import org.openqa.selenium.By;
 import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.support.ui.ExpectedConditions;
-import org.openqa.selenium.support.ui.WebDriverWait;
+//import org.openqa.selenium.support.ui.ExpectedConditions;
+//import org.openqa.selenium.support.ui.WebDriverWait;
 //import org.openqa.selenium.support.ui.Select;
 
 import utilsCrm.crmFunctions;
